452a6f4877f7df868235fee189bfb4a098e39148
SondreMelhus <Sondre.Melhus@no.experis.com>
2022-09-16 13:44:29 +0200

    Project setup

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+export default function Footer () {
+
+    return (
+        <div>
+            <footer>
+                <div>
+                    Footer comp
+                </div>
+            </footer>
+        </div>
+    )
+}
---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+export default function Header () {
+
+    return (
+        <div>
+            <header>
+                <div>
+                    Header comp
+                </div>
+            </header>
+        </div>
+    )
+}
