f51659d23be2219b86244313e80510a37cca15de
Karoline Øijorden <karoline.oijorden@no.experis.com>
2022-09-16 15:13:40 +0200

    Add submit form

--- src/components/Translator.js
+++ src/components/Translator.js
@@ -1,12 +1,28 @@
-import React from "react"
+import { useState } from "react"
 import '../styles/index.css'
 
 export default function Translator () {
 
+    const [text, translateText] = useState("");
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        alert(`The text you want to translate is: ${text}`); {/*remove this line when done with testing */}
+      }
+
     return (
         <div>
             <div>
-                Translator comp
+                <form onSubmit={handleSubmit}>
+                    <label>Translate:
+                    <input 
+                        type="text" 
+                        value={text}
+                        onChange={(e) => translateText(e.target.value)}
+                    />
+                    </label>
+                    <input type="submit" />
+                </form>
             </div>
         </div>
     )
