8a4a1044629c7c2250443ddc7f6b4389299b208d
SondreMelhus <Sondre.Melhus@no.experis.com>
2022-09-16 13:58:34 +0200

    Project setup

--- src/index.js
+++ src/index.js
@@ -7,26 +7,19 @@ import LogIn from './components/LogIn';
 import Translator from './components/Translator';
 import Profile from './components/Profile';
 import Footer from './components/Footer';
-/*
-import { 
-  Header,
-  LogIn,
-  Translator,
-  Profile,
-  Footer
-} from './components';
-*/
 
 
-ReactDOM.render(
-  <Router >
-    <Header />
-    <Routes>
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Router >
+     <Header />
+      <Routes>
       <Route path="/" element={<LogIn />} />
       <Route path="/Translator" element={<Translator />} />
       <Route path="/Profile" element={<Profile />} />
-    </Routes>
-    <Footer />
-  </Router>,
-  document.getElementById('root')
+      </Routes>
+      <Footer />
+    </Router>
+  </React.StrictMode>
 );
