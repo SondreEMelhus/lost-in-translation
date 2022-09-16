3178d468d8596acfc951122b0ba5a57ab53138aa
SondreMelhus <Sondre.Melhus@no.experis.com>
2022-09-16 13:51:51 +0200

    Project setup

--- src/index.js
+++ src/index.js
@@ -2,13 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import './index.css';
-import {
+import Header from './components/Header';
+import LogIn from './components/LogIn';
+import Translator from './components/Translator';
+import Profile from './components/Profile';
+import Footer from './components/Footer';
+/*
+import { 
   Header,
   LogIn,
   Translator,
   Profile,
   Footer
 } from './components';
+*/
 
 
 ReactDOM.render(
