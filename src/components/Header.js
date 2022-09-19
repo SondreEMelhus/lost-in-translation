dbd9b400bf75320858e3edd81ca876dadc43d87f
vegardgunnarson <110391678+vegardgunnarson@users.noreply.github.com>
2022-09-19 10:56:23 +0200

    feat: add sister font

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,22 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import '../styles/index.css'
+import '../styles/Header.css'
+import logo from '../assets/Logo.png'
 
 export default function Header () {
 
     return (
         <div className="header">
-            <header>
-                <div>
-                    <NavLink to="/Profile">Profile</NavLink>
-                </div>
-            </header>
+                    <div>
+                    <img src={logo} className="headerLogo" alt="logo"/>
+                    </div>
+                    <div className="headerTitle">
+                        <h1>Lost in translation</h1>
+                    </div>
+                    <div className="headerProfileLink">
+                        <NavLink to="/Profile"><img src={logo} className="profileButton" alt="logo"/></NavLink>
+                    </div>
         </div>
     )
 }
