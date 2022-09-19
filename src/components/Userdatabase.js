964e004f17a085f27010c56df94350188b1c779e
SondreMelhus <Sondre.Melhus@no.experis.com>
2022-09-19 09:36:03 +0200

    Implemented login functionality

---
+++ src/components/Userdatabase.js
@@ -0,0 +1,14 @@
+export default function UserDatabase () {
+    this.database = new Map();
+    this.database.set('Sondre', '123');
+
+    this.checkForUserInDatabase = (username, password) => {
+        if (this.database.has(username)) {
+            return this.database.get(username) == password ? true : false;
+        }
+    }
+
+    this.addUser = (username, password) => {
+        this.database.set(username, password);
+    }
+}
