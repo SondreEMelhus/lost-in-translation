54a17c95f588da301c9d7645cc8aa9f46684bffa
vegardgunnarson <110391678+vegardgunnarson@users.noreply.github.com>
2022-09-19 13:31:24 +0200

    fix: add profilelink to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,7 +8,10 @@ import profileLogo from '../assets/profile.svg'
 export default function Header () {
 
     function getUser(){
-        return "  Sondre  ";
+        return "Sondre";
+    }
+    function getLength(){
+        return ((getUser().length)*10+10).toString()+"px";
     }
 
     return (
@@ -21,7 +24,7 @@ export default function Header () {
                     </div>
                     <div className="headerProfileLink">
                         <NavLink className="profileLink" to="/Profile">
-                            <p className="profileText">{getUser()}</p>
+                            <p className="profileText" style={{width: getLength()}} >{getUser()}</p>
                             <img src={profileLogo} className="profileImage" alt="logo"/>
                         </NavLink>
                     </div>
