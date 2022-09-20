9d4a6f85829e2be493f1e9747c355c7909d8aa17
SondreMelhus <Sondre.Melhus@no.experis.com>
2022-09-20 09:26:49 +0200

    Added methods to store and retrive users from localstorage

--- src/components/UserHandler.js
+++
@@ -1,9 +0,0 @@
-//Function used to store a user in localstorage
-export function storeUser() {
-    
-}
-
-//Function used to retrive a user from localstorage
-export function retriveUser() {
-    return JSON.parse(localStorage.getItem('user'));
-}
